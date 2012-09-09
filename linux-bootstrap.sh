4b1265efb9bc94443bc310e91ab2216a1e61a0f8
Michiel Roos <michiel@maxserv.nl>
2012-09-09 12:26:03 +0200

    Check for file / link existence before removal

--- linux-bootstrap.sh
+++ linux-bootstrap.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-rm -rf ~/.vim
-rm ~/.vimrc
+if [ -e ~/.vim ] || [ -L ~/.vim ]
+then
+	rm -rf ~/.vim
+fi
+if [ -e ~/.vimrc ] || [ -L ~/.vimrc ]
+then
+	rm -f ~/.vimrc
+fi
 ln -s ${PWD##*/}/.vim ~/.vim
 ln -s ${PWD##*/}/.vimrc ~/.vimrc
 git submodule update --init
