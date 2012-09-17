e2dc183cccded1ea613c31e0acd74c7c3e1f8d6c
Michiel Roos <michiel@maxserv.nl>
2012-09-17 15:00:10 +0200

    Make sure Command-T is compiled using system ruby version

--- linux-bootstrap.sh
+++ linux-bootstrap.sh
@@ -8,6 +8,7 @@ else
 	home=$1
 fi
 
+# Create symlinks
 if [ -e $home/.vim ] || [ -L $home/.vim ]
 then
 	rm -rf $home/.vim
@@ -16,6 +17,13 @@ if [ -e $home/.vimrc ] || [ -L $home/.vimrc ]
 then
 	rm -f $home/.vimrc
 fi
-ln -s ${PWD##*/}/.vim $home/.vim
-ln -s ${PWD##*/}/.vimrc $home/.vimrc
+ln -s $home/${PWD##*/}/.vim $home/.vim
+ln -s $home/${PWD##*/}/.vimrc $home/.vimrc
+
+# Initialize submodules
 git submodule update --init
+
+# Compile command-t for our ruby version
+cd $home/${PWD##*/}/.vim/bundle/Command-T/ruby/command-t
+ruby extconf.rb
+make
