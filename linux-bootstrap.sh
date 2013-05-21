19934571f36554f1174965059620dc7dc636a995
Remco Overdijk <remco@maxserv.nl>
2013-05-21 12:22:56 +0200

    Added powerline-shell as a submodule for this project and modified the bootstrap accordingly

--- linux-bootstrap.sh
+++ linux-bootstrap.sh
@@ -10,12 +10,11 @@ fi
 #Install prerequisites
 if [ ! -d $home/powerline-shell ]
 then
-	cd $home && git clone https://github.com/milkbikis/powerline-shell
-	cd $home/powerline-shell && ./install.py
+	cd $home/${PWD##*/}/powerline-shell && ./install.py
   #Patch the arrow character in the font, as it is currently broken, see:
   #https://github.com/milkbikis/powerline-shell/pull/69
 	curl https://github.com/milkbikis/powerline-shell/pull/69.patch | git apply
-	ln -s $home/powerline-shell/powerline-shell.py $home/powerline-shell.py
+	ln -s $home/${PWD##*/}/powerline-shell/powerline-shell.py $home/powerline-shell.py
 fi
 if [ ! -d $home/.oh-my-zsh ]
 then
