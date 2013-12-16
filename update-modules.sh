124ae144e010f1854e6d9608e4d9c983d3326488
Remco Overdijk <remco@maxserv.nl>
2013-12-16 10:06:41 +0100

    Updated the updater; Some modules now have submodules which need to be updated as well

--- update-modules.sh
+++ update-modules.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 git submodule foreach git fetch --all
 git submodule foreach git reset --hard origin/master
+git submodule update --init --recursive
 cd .vim/bundle/powerline
 git reset --hard origin/develop
 cd ../../../
