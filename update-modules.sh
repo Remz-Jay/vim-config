09ca1b876269ce5f00c2e3822b30b75fbec7efaa
Remco Overdijk <remco@maxserv.com>
2014-03-10 10:33:28 +0100

    Expanded updater to preserve powerline-shell changes

--- update-modules.sh
+++ update-modules.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
+cd powerline-shell
+git stash
+cd ..
 git submodule update --init --recursive
 git submodule foreach git fetch --all
 git submodule foreach git reset --hard origin/master
 cd .vim/bundle/powerline
 git reset --hard origin/develop
 cd ../../../powerline-shell
+git stash pop
 ./install.py
 cd ../
 fc-cache -vf ./powerline-fonts
